52bfdee477a26148a40d07fa1943a64199f321b2
William Whitaker <william@williamwhitaker.dev>
2025-04-12 10:53:02 -0400

    feat: improved artworks object

---
+++ src/_data/artworks.js
@@ -0,0 +1,28 @@
+const imgPath = 'assets/images';
+
+export default [
+  {
+    painting: [
+      {
+        title: 'painting-1',
+        path: `${imgPath}green-square.webp`
+      },
+      {
+        title: 'painting-2',
+        path: `${imgPath}/blue-square.webp`
+      }
+    ]
+  },
+  {
+    paper: [
+      {
+        title: 'drawing-1',
+        path: `${imgPath}/violet-square.webp`
+      },
+      {
+        title: 'drawing-2',
+        path: `${imgPath}/red-square.webp`
+      }
+    ]
+  }
+];
