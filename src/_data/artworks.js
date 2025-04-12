df6c3e4509a9146b74c2907c9e223440eed6fc03
William Whitaker <william@williamwhitaker.dev>
2025-04-12 11:33:30 -0400

    feat: stub pagination for art categories

--- src/_data/artworks.js
+++ src/_data/artworks.js
@@ -2,27 +2,9 @@ const imgPath = 'assets/images';
 
 export default [
   {
-    painting: [
-      {
-        title: 'painting-1',
-        path: `${imgPath}green-square.webp`
-      },
-      {
-        title: 'painting-2',
-        path: `${imgPath}/blue-square.webp`
-      }
-    ]
+    category: 'painting'
   },
   {
-    paper: [
-      {
-        title: 'drawing-1',
-        path: `${imgPath}/violet-square.webp`
-      },
-      {
-        title: 'drawing-2',
-        path: `${imgPath}/red-square.webp`
-      }
-    ]
+    category: 'drawing'
   }
 ];
