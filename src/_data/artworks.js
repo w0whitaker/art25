623804bdf47dc6e9519e357637c211147136e82b
William Whitaker <william@williamwhitaker.dev>
2025-04-14 16:43:16 -0400

    feat: add new structure for art posts

--- src/_data/artworks.js
+++ src/_data/artworks.js
@@ -1,12 +1,31 @@
 const imgPath = 'assets/images';
 
-export default [
+export const paintings = [
   {
-    category: 'paintings',
-    years: ['2020', '2021', '2022', '2023', '2024']
+    title: 'test-painting-1',
+    path: '/assets/images/blue-square.webp'
   },
   {
-    category: 'works on paper',
-    years: ['2020', '2021', '2022', '2023', '2024']
+    title: 'test-painting-2',
+    path: '/assets/images/red-square.webp'
+  },
+  {
+    title: 'test-painting-3',
+    path: '/assets/images/green-square.webp'
+  }
+];
+
+export const worksOnPaper = [
+  {
+    title: 'test-drawing-1',
+    path: `${imgPath}/blue-square.webp`
+  },
+  {
+    title: 'test-drawing-2',
+    path: `${imgPath}/red-square.webp`
+  },
+  {
+    title: 'test-drawing-3',
+    path: `${imgPath}/green-square.webp`
   }
 ];
