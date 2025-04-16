7ec4fbaa17c5d2c64f5f44074c0bc64919a2dc0e
William Whitaker <william@williamwhitaker.dev>
2025-04-16 08:49:21 -0400

    chore: add test data

--- src/_data/artworks.js
+++ src/_data/artworks.js
@@ -1,31 +1,49 @@
-const imgPath = 'assets/images';
+const imgPath = '/assets/images';
 
 export const paintings = [
   {
     title: 'test-painting-1',
-    path: '/assets/images/blue-square.webp'
+    path: `${imgPath}/blue-square.webp`,
+    medium: 'acrylic on paper',
+    dims: '38x25',
+    year: '2020'
   },
   {
     title: 'test-painting-2',
-    path: '/assets/images/red-square.webp'
+    path: `${imgPath}/red-square.webp`,
+    medium: 'oil on canvas',
+    dims: '18x32',
+    year: '2012'
   },
   {
     title: 'test-painting-3',
-    path: '/assets/images/green-square.webp'
+    path: `${imgPath}/green-square.webp`,
+    medium: 'acrylic on paper',
+    dims: '38x25',
+    year: '2022'
   }
 ];
 
 export const worksOnPaper = [
   {
     title: 'test-drawing-1',
-    path: `${imgPath}/blue-square.webp`
+    path: `${imgPath}/blue-square.webp`,
+    medium: 'graphite on paper',
+    dims: '36x24',
+    year: '2020'
   },
   {
     title: 'test-drawing-2',
-    path: `${imgPath}/red-square.webp`
+    path: `${imgPath}/red-square.webp`,
+    medium: 'collage on paper',
+    dims: '5x5',
+    year: '2021'
   },
   {
     title: 'test-drawing-3',
-    path: `${imgPath}/green-square.webp`
+    path: `${imgPath}/green-square.webp`,
+    meduim: 'collage on paper',
+    dims: '5x5',
+    year: '2021'
   }
 ];
