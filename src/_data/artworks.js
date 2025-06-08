0bda6a3cb1dd356c670fce496db0e0d2e9ebf627
William Whitaker <william@williamwhitaker.dev>
2025-06-08 06:37:58 -0400

    content: artwork page setup

--- src/_data/artworks.js
+++ src/_data/artworks.js
@@ -1,28 +1,78 @@
-const imgPath = 'assets/images';
+const imgPath = '/assets/images';
+const sizeOne = '38 x 25';
 
-export default [
-  {
-    paintings: [
-      {
-        title: 'painting-1',
-        path: `${imgPath}green-square.webp`
-      },
-      {
-        title: 'painting-2',
-        path: `${imgPath}/blue-square.webp`
-      }
-    ]
-  },
-  {
-    paper: [
-      {
-        title: 'drawing-1',
-        path: `${imgPath}/violet-square.webp`
-      },
-      {
-        title: 'drawing-2',
-        path: `${imgPath}/red-square.webp`
-      }
-    ]
-  }
-];
+export default {
+  paintings: [
+    {
+      title: 'ULG2094',
+      year: 2020,
+      path: `${imgPath}/UlG2094.webp`,
+      medium: 'acrylic on paper',
+      dims: `${sizeOne}`
+    },
+    {
+      title: 'ULG2106',
+      year: 2021,
+      path: `${imgPath}/ULG2106.jpg`,
+      medium: 'acrylic on paper',
+      dims: `${sizeOne}`
+    },
+    {
+      title: 'ULG2110',
+      year: 2021,
+      path: `${imgPath}/ULG2110.webp`,
+      medium: 'acrylic on paper',
+      dims: `${sizeOne}`
+    },
+    {
+      title: 'ULG2114',
+      year: 2021,
+      path: `${imgPath}/ULG2114.webp`,
+      medium: 'acrylic on paper',
+      dims: `${sizeOne}`
+    },
+    {
+      title: 'ULG2115',
+      year: 2021,
+      path: `${imgPath}/ULG2115.webp`,
+      medium: 'acrylic on paper',
+      dims: `${sizeOne}`
+    },
+    {
+      title: 'ULG2116',
+      year: 2021,
+      path: `${imgPath}/ULG2116.webp`,
+      medium: 'acrylic on paper',
+      dims: `${sizeOne}`
+    },
+    {
+      title: 'ULG2117',
+      year: 2021,
+      path: `${imgPath}/ULG2117.webp`,
+      medium: 'acrylic on paper',
+      dims: `${sizeOne}`
+    }
+  ],
+  worksOnPaper: [
+    {
+      title: 'UD2001',
+      year: 2020,
+      path: `${imgPath}/UD2001.webp`
+    },
+    {
+      title: 'UD2004',
+      year: 2020,
+      path: `${imgPath}/UD2004.webp`
+    },
+    {
+      title: 'UD2005',
+      year: 2020,
+      path: `${imgPath}/UD2005.webp`
+    },
+    {
+      title: 'UD2006',
+      year: 2020,
+      path: `${imgPath}/UD2006.webp`
+    }
+  ]
+};
